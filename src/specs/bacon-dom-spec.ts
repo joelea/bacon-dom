afbd2ff3d5427d78fdbacfb8ad18a65c8d91a0bc
Joe Lea <joeleapublic@gmail.com>
2015-10-28 22:11:47 +0000

    Add spec checking actually adding some HTML

--- src/specs/bacon-dom-spec.ts
+++ src/specs/bacon-dom-spec.ts
@@ -1,18 +1,24 @@
 /// <reference path="../typings/tsd.d.ts"/>
 
 import chai = require("chai");
-import mockBrowser = require("mock-browser");
 import Bacon = require("baconjs");
 import attach = require("../main/bacon-dom");
+import h = require("virtual-dom/h")
 
 var expect = chai.expect;
-
-var document = mockBrowser.mocks.MockBrowser.createDocument();
 
 describe("bacon-dom", () => {
     it("triggers no dom changes when the observable does not change", () => {
         var emptyDomStream = new Bacon.Bus();
         attach(emptyDomStream).to(document.body);
         expect(document.body.children.length).to.equal(0);
+    })
+
+    it("contains the initial dom state", () => {
+        var initialDom = Bacon.once(h('.hello-world', []));
+        attach(initialDom).to(document.body);
+
+        expect(document.body.getElementsByClassName('.hello-world'))
+            .to.have.length(1);
     })
 })
