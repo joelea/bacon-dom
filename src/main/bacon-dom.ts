da2718b93a4676df2e16a39e287c790966b2304e
Joe Lea <joeleapublic@gmail.com>
2015-11-08 11:19:13 +0000

    Simplify code with the Bacon diff method

--- src/main/bacon-dom.ts
+++ src/main/bacon-dom.ts
@@ -4,30 +4,19 @@ import vDom = require('virtual-dom');
 import diff = require('virtual-dom/diff');
 import patch = require('virtual-dom/patch');
 import createElement = require('virtual-dom/create-element');
-import Stream = require('baconjs')
+import h = require('virtual-dom/h');
+import Stream = require('baconjs');
 
 function attach<E>(vtreeStream:Bacon.EventStream<E,vDom.VNode>) {
     return {
         to: (parentElement:Element) => {
-            var initialNode = vtreeStream
-                .take(1)
-                .map(createElement)
-                .toProperty()
-
-            initialNode.onValue((rootNode) => { parentElement.appendChild(rootNode); })
+            var initialElement = h('.placeholder');
+            var rootNode = parentElement.appendChild(createElement(initialElement))
 
             vtreeStream
-                .slidingWindow(2, 2)
-                .map((trees) => { return diff(trees[0], trees[1]) })
-                .combine(initialNode, (patches, rootNode) => {
-                    return {
-                        patches: patches,
-                        rootNode: rootNode
-                    }
-                }).onValue((frame) => {
-                    window.requestAnimationFrame(() => {
-                        patch(frame.rootNode, frame.patches)
-                    })
+                .diff(initialElement, diff)
+                .onValue( (elementDiff) => {
+                    patch(rootNode, elementDiff)
                 })
         }
     }
